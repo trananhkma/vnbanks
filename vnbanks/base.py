a3d50a020bfac8c477db3642839207d6ba685718
Hung Nguyen Viet <hvn@familug.org>
2015-07-27 22:26:03 +0800

    add base for base classes

---
+++ vnbanks/base.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+
+class Bank(object):
+    def __init__(self, name, **kwargs):
+        self.name = name
+
+    def deposite_rate(self):
+        pass
+
+    def loan_rate(self):
+        pass
